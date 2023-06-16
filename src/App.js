e5d9632d4c06e62ed8e4c00c11ab68d9d79acfdd
Air128731 <henry.scott0x8080@gmail.com>
2023-06-16 05:39:26 -0400

    contact us added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { List } from '@mui/material';
 import './App.css';
 import Landing from './components/Landing';
 import Listings from './components/Listings';
+import Contactus from './components/Contactus';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />}></Route>
         <Route path='/listingproperty' element={<Listings />}></Route>
+        <Route path='/contact' element={<Contactus />}></Route>
       </Routes>
     </BrowserRouter>
   );
